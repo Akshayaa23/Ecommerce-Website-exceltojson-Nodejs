8b104e9cad1d89d6e4a040a1c77c1c06056b4010
Akshayaa23 <105343388+Akshayaa23@users.noreply.github.com>
2022-09-12 13:21:04 +0530

    code update

--- controllers/products.js
+++ controllers/products.js
@@ -1,31 +1,66 @@
-const Category = require('../database/models/category')
-const Product = require('../database/models/product')
-const mongoose = require('mongoose')
-const jwt = require('jsonwebtoken')
+const Products = require('../database/models/product')
+const excelToJson = require('convert-excel-to-json');
 
+// upload
+const uploadproduct = async(req,res,next) => {
+  try{
+      const file = req.file.path
+      let excelData = excelToJson({
+        sourceFile: file,
+          header: {
+            rows: 1
+          },
+          columnToKey: {
+            A:'ids',
+            B:'name',
+            C:'description',
+            D:'richDescription',
+            E:'image',
+            F:'brand',
+            G:'price',
+            H:'category',
+            I:'countInStock',
+            J:'rating',
+            K:'numRevies',
+          }
+    })
+    console.log(excelData)
+    
+    excelData = await Products.insertMany(excelData.Sheet1)
+    // excelData = await Products.findOneAndUpdate({ids : excelData.map(a=>a.ids)},
+    // {$set: {$or:[{name: excelData.map(a=>a.name)},{price: excelData.map(a=>a.price)}]}},{upsert : true})
 
-//get all products
-const index = async(req,res) => {
-    try{
-        const productList = await Product.find().populate('category')
-        //.select('name image -_id')
-        if(productList){
-            res.status(200).json({
-                data: productList
-            })
-        }
-    } catch(error){
-        res.status(400).send({success:false,msg:error.message})
-    }
+    res.json({
+        status: 200,
+        message: 'Added successfully',
+        excelData
+    })
+  }catch(error){ res.status(400).json({
+      message:error.message
+    })
+  }
 }
 
+//get products
+const getProduct = async(req,res) => {
+  try{
+    result = await Products.find()
+    res.status(200).json({
+      message:'product listed',
+      result
+    })
+
+  }catch(error){
+    res.status(400).json({msg:error})
+  }
+}
 
 
 //get single product
 const show = async(req,res) => {
     try{
         let id = req.params.id
-        const productId = await Product.findById({_id :id}).populate('category')
+        const productId = await Products.findById({_id :id})
         if(productId){
             res.status(200).json({
                 data: productId
@@ -36,78 +71,11 @@ const show = async(req,res) => {
     }
 }
 
-//add new product
-const store = async(req,res) => {
-    try{
-        const category = await Category.findById(req.body.category)
-        if(!category){
-            return res.status(400).json({error:'invalid category'})
-        }
-        let product = new Product({
-            name: req.body.name,
-            description: req.body.description,
-            richDescription: req.body.richDescription,
-            image: req.body.image,
-            brand: req.body.brand,
-            price: req.body.price,
-            category: req.body.category,
-            countInStock: req.body.countInStock,
-            rating: req.body.rating,
-            numReviews: req.body.numReviews,
-            isFeatured: req.body.isFeatured
-        })
-        await product.save()
-        return res.status(200).json({
-            message:'product added...',
-            data:product
-        })
-    }catch(error){
-        res.status(400).send({success:false,msg:error.message})
-    }
-}
-
-//update a product
- const update = async(req,res)=> {
-    try{
-        if(!mongoose.isValidObjectId(req.params.id)){
-            res.status(400).json({error: 'invalid product id'})
-        }
-        const category = await Category.findById(req.body.category)
-        if(!category){
-            return res.status(400).json({error:'invalid category'})
-        }
-        const product = await Product.findByIdAndUpdate(
-            req.params.id,
-            {
-                name: req.body.name,
-                description: req.body.description,
-                richDescription: req.body.richDescription,
-                image: req.body.image,
-                brand: req.body.brand,
-                price: req.body.price,
-                category: req.body.category,
-                countInStock: req.body.countInStock,
-                rating: req.body.rating,
-                numReviews: req.body.numReviews,
-                isFeatured: req.body.isFeatured  
-            }
-        )  
-        if(product){
-            return res.status(200).json({
-                message:"product updated",
-                data: product
-            })
-        } 
-        }catch(error){
-            res.status(400).send({success:false,msg:error.message})
-        }
-}
-
 //delete a product
 const destroy = async(req,res) => {
     try{
         id = req.params.id
-        await Product.findByIdAndRemove({_id: id})
+        await Products.findByIdAndRemove({_id: id})
         return res.status(200).json({
             message: 'product Deleted...'
         })
@@ -120,7 +88,7 @@ const destroy = async(req,res) => {
 const totalCount = async(req,res) => {
     try{
         
-        const productcount = await Product.countDocuments()
+        const productcount = await Products.countDocuments()
         if(productcount){
             res.status(200).json({
                 productCount: productcount
@@ -135,7 +103,7 @@ const totalCount = async(req,res) => {
 const getFeaturedlimit = async(req,res) => {
     try{
         const count = req.params.count?req.params.count:0
-        const products = await Product.find({isFeatured:true}).limit(+count)
+        const products = await Products.find({isFeatured:true}).limit(+count)
         if(products){
             res.status(200).json({
                 limitisfeaturedProducts: products
@@ -147,8 +115,6 @@ const getFeaturedlimit = async(req,res) => {
 }
 
 
-
-
 module.exports = {
-    index,show,store,update,destroy,totalCount,getFeaturedlimit
+    show,destroy,totalCount,getFeaturedlimit,uploadproduct,getProduct
 }
